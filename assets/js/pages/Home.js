41b7574fa473296cef6dfa51d8600300cfc2cd3d
Rokaskl <rokasklkl@gmail.com>
2019-11-08 15:33:19 +0200

    react init

---
+++ assets/js/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import gif from "..//../img/tumblr_ovx39d2RQo1rx3k87o1_400.gif";
+
+export default function home() {
+  return (
+    <div className="center">
+      <img src={gif} alt="coming soon..." />
+    </div>
+  );
+}
